6100ac3c5bca16f4fd56497001c16b8bae846903
Matthew6596 <masatterfield@email.neit.edu>
2022-12-01 10:48:27 -0500

    Minor changes to asteroid avoider

--- asteroidAvoider/asteroidAvioder.js
+++ asteroidAvoider/asteroidAvioder.js
@@ -3,16 +3,19 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var timer = requestAnimationFrame(main);
 
-var numAster = 20;
+var numAster = 10;
+var aLowSpd = 1;
+var aHighSpd = 5;
 var asteroids = [];
 var player = new Player();
 
 //Istantiation
 for(i=0;i<numAster;i++){
-    var col = randNum(3,0);
+    var col = randNum(4,0);
     if(col<1){col="red";}
     else if(col<=2){col="white";}
-    else if(col<=3){col="blue";}
+    else if(col<=3){col="orange";}
+    else if(col<=4){col="yellow";}
     asteroids[i] = new Asteroid(col);
 }
 
@@ -25,7 +28,7 @@ function Asteroid(col){
     this.radius = randNum(2,15);
     this.x = randNum(this.radius,canvas.width-this.radius);
     this.y = randNum(this.radius,canvas.height-this.radius) - canvas.height;
-    this.vy = randNum(5,10);
+    this.vy = randNum(aLowSpd,aHighSpd);
     this.color = col;
 
     this.draw = function(){
@@ -50,6 +53,7 @@ function Player(){
     this.draw = function(){
         ctx.save();
         ctx.strokeStyle = this.color;
+        ctx.lineWidth = "2";
         ctx.beginPath();
         ctx.moveTo(this.x,this.y);
         ctx.lineTo(this.x+this.w,this.y);
@@ -60,6 +64,8 @@ function Player(){
         ctx.restore();
     }
     this.move = function(){
+        if(this.x>=canvas.width-this.w){this.kD=false;}
+        if(this.x<=0){this.kA=false;}
         if(this.kD){this.x+=this.spd;}
         if(this.kA){this.x-=this.spd;}
     }
@@ -79,20 +85,24 @@ function randNum(low,high){
     return (Math.random()*(high-low)+low);
 }
 function asteroidManage(n){
-    if((asteroids[n].y+(asteroids[n].radius*2))>canvas.height){
-        if((asteroids[n].x<player.x+player.w)&&asteroids[n].x>player.x&&asteroids[n].y>(player.y-player.w)){console.log("hit!");}
-
-        var colorN = randNum(3,0)
+    if((asteroids[n].x>(player.x-asteroids[n].radius)&&asteroids[n].x<(player.x+player.w+asteroids[n].radius)&&asteroids[n].y>player.y-player.w/1.2)){gameOver();}
+    if(asteroids[n].y-asteroids[n].radius>canvas.height){
+        var colorN = randNum(4,0)
         asteroids[n].radius = randNum(2,15);
         asteroids[n].x = randNum(asteroids[n].radius,canvas.width-asteroids[n].radius);
         asteroids[n].y = -(asteroids[n].radius+100);
-        asteroids[n].vy = randNum(5,10);
+        asteroids[n].vy = randNum(aLowSpd,aHighSpd);
         if(colorN<1){asteroids[n].color="red";}
         else if(colorN<=2){asteroids[n].color="white";}
-        else if(colorN<=3){asteroids[n].color="blue";}
+        else if(colorN<=3){asteroids[n].color="orange";}
+        else if(colorN<=4){asteroids[n].color="yellow";}
     }
     asteroids[n].y+=asteroids[n].vy;
     asteroids[n].draw();
+}
+function gameOver(){
+    console.log("Hit!");
+    player.w = 1;
 }
 //Main
 function main(){
