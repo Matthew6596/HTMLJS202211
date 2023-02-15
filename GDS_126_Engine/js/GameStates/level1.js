871ec7cd1cc4ecbcc2c3d76e42275e2169b304ad
Matthew6596 <masatterfield@email.neit.edu>
2023-02-15 14:44:30 -0500

    end of class week 6

--- GDS_126_Engine/js/GameStates/level1.js
+++ GDS_126_Engine/js/GameStates/level1.js
@@ -1,20 +1,20 @@
 
 /*------------Use this if you want to implement States---------------*/
 var gravity = 1;
-var friction = {x:.85,y:.97}
+var friction = {x:.85,y:.97};
 
 var stage = new GameObject({width:canvas.width, height:canvas.height});
 
 //Avatar
-var wiz = new GameObject({y:canvas.height/2-64,width:64, height:192, spriteData:playerData}).makeSprite(playerData)
-wiz.force=1
+var wiz = new GameObject({y:canvas.height/2-64,width:64, height:192, spriteData:playerData}).makeSprite(playerData);
+wiz.force=1;
 
 //The ground
-var ground = new GameObject({width:canvas.width*10, x:canvas.width*10/2,height:64,y:canvas.height-32, color:"green"})
-ground.img.src=`images/ground.png`
+var ground = new GameObject({width:canvas.width*10, x:canvas.width*10/2,height:64,y:canvas.height-32, color:"green"});
+ground.img.src=`images/ground.png`;
 
 //A platform
-var plat = new GameObject({width:256, height:64,y:canvas.height-200, color:"green"})
+var plat = new GameObject({width:256, height:64,y:canvas.height-200, color:"green"});
 
 //A level object when it is moved other objects move with it.
 var level = new GameObject({x:0,y:0});
@@ -28,36 +28,36 @@ var caveBack = new Grid(caveBackData, {world:level, x:1024, tileHeight:64, tileW
 //cave hitbox grid
 var caveHit = new Grid(caveHitData, {world:level, x:1024, tileHeight:64, tileWidth:64});
 
-var leftBorder = new GameObject({x:0, height:canvas.height, world:level})
+var leftBorder = new GameObject({x:0, height:canvas.height, world:level});
 
 //This is a group used for collisions
 var g1 = new Group();
 g1.color= `rgb(251,0,254)`;
 
 //Adds items to a group
-g1.add([ground,leftBorder, caveHit.grid])
+g1.add([ground,leftBorder, caveHit.grid]);
 
 //removes items from a group
 //g1.remove([plat, cave.grid])
 
 //Used to draw the rectangles
 var rects = new Group();
-rects.add([ground,plat])
+rects.add([ground,plat]);
 
 //used to render the sprites
 var sprites = new Group();
-sprites.add([caveBack.grid])
+sprites.add([caveBack.grid]);
 
-var front = new Group()
-front.add([cave.grid])
+var front = new Group();
+front.add([cave.grid]);
 
 //list of items to be rendered in the level
 var levelItems=new Group();
 levelItems.add([caveBack.grid, ground, plat, cave.grid]);
 
 //Very back background
-var sky = new GameObject({width:canvas.width, height:canvas.height, color:"cyan"})
-sky.img.src = `images/sky.png`
+var sky = new GameObject({width:canvas.width, height:canvas.height, color:"cyan"});
+sky.img.src = `images/sky.png`;
 
 /*
  	//Not used, unless you want a 4th level of paralax
@@ -66,16 +66,16 @@ sky.img.src = `images/sky.png`
 */
 
 //repeating background
-var rbg = new GameObject({x:level.x, y:level.y, width:1024, height:512})
-rbg.img.src=`images/hills.png`
+var rbg = new GameObject({x:level.x, y:level.y, width:1024, height:512});
+rbg.img.src=`images/hills.png`;
 
 //middleground
-var bg = new GameObject({x:level.x,y:level.y, width:canvas.width*4, height:canvas.height})
-bg.img.src=`images/bgfull.png`
+var bg = new GameObject({x:level.x,y:level.y, width:canvas.width*4, height:canvas.height});
+bg.img.src=`images/bgfull.png`;
 
 /*------------------vvBULLET STUFFvv----------------------*/
 
-var bullets=[]
+var bullets=[];
 var canShoot=true;
 var shotTimer = 0;
 var shotDelay = 21;
@@ -83,11 +83,11 @@ var currentBullet = 0;
 
 for(let i=0; i<100; i++)
 {
-	bullets[i] = new GameObject({width:2, height:2})
+	bullets[i] = new GameObject({width:2, height:2});
 	//bullets[i].img.src="images/mrt.jpg"
-	bullets[i].makeSprite(playerData)
-	bullets[i].y=-10000
-	bullets[i].changeState(`projectile`)
+	bullets[i].makeSprite(playerData);
+	bullets[i].y=-10000;
+	bullets[i].changeState(`projectile`);
 }
 
 //console.log(bullets)
@@ -144,7 +144,7 @@ gameStates[`level1`] = function()
 	if(keys[`S`]&& !chargin && attCool<0 && wiz.canJump)
 	{
 		wiz.top={x:0,y:0};
-		wiz.changeState(`crouch`)
+		wiz.changeState(`crouch`);
 	}
 	else
 	{
@@ -161,7 +161,7 @@ gameStates[`level1`] = function()
 			}else{
 				wiz.changeState(`walk`);
 			}
-			wiz.vx += wiz.force
+			wiz.vx += wiz.force;
 			
 		}
 		
@@ -200,7 +200,7 @@ gameStates[`level1`] = function()
 	shotTimer--;
 	if(shotTimer <=0)
 	{
-		canShoot=true
+		canShoot=true;
 	}
 	else
 	{
@@ -233,7 +233,7 @@ gameStates[`level1`] = function()
 			
 			if(currentBullet>=bullets.length)
 			{
-				currentBullet=0
+				currentBullet=0;
 			}
 
 		}
@@ -248,20 +248,20 @@ gameStates[`level1`] = function()
 			}
 			chargeTime++;
 			if(chargeTime>=20){
-				wiz.changeState(`charging`)
+				wiz.changeState(`charging`);
 			}
 		}
 	}
 	else
 	{
-		shotTimer=0
+		shotTimer=0;
 	}
 	
 	//-----Player movement-----///
-	wiz.vy+= gravity
-	wiz.vx *= friction.x
-	wiz.vy *= friction.y
-	wiz.x += Math.round(wiz.vx)
+	wiz.vy+= gravity;
+	wiz.vx *= friction.x;
+	wiz.vy *= friction.y;
+	wiz.x += Math.round(wiz.vx);
 	if(chargin){gravity=0.02;}
 	else if(attCool>=0){gravity=0.4;}
 	else{gravity=1;}
@@ -336,16 +336,16 @@ gameStates[`level1`] = function()
 	//Sets up pattern for the ground
 	var groundPattern = context.createPattern(ground.img, `repeat`);
 	//Applies pattern to ground and platform
-	ground.color = groundPattern
-	plat.color = groundPattern
+	ground.color = groundPattern;
+	plat.color = groundPattern;
 
 	//Sets up pattern for the sky
 	var skyPattern = context.createPattern(sky.img, `repeat`);
 	//Applies pattern to sky
-	sky.color = skyPattern
+	sky.color = skyPattern;
 
 	//renders the sky
-	sky.render()
+	sky.render();
 	
 	//Renders the repeating background
 	rbg.drawStaticImage([0,0]);
@@ -359,7 +359,7 @@ gameStates[`level1`] = function()
 	//rbg.render(`drawStaticImage`, [0,0])
 
 	//renders the objects in the rect group
-	rects.render(`drawRect`, [0,0,100,100])
+	rects.render(`drawRect`, [0,0,100,100]);
 	
 	/*----Used for debugging----*/
 	/*context.beginPath()
@@ -371,14 +371,14 @@ gameStates[`level1`] = function()
 	sprites.play().render(`drawSprite`);
 
 	//renders player
-	wiz.play(function(){return}).drawSprite()
+	wiz.play(function(){return}).drawSprite();
 	
 	//Moves, checks collision and renders projectiles.
 	for(let i=0; i<bullets.length; i++)
 	{
 		//if(bullets[i].overlap(stage)) bullets[i].vy+=1;
-		bullets[i].move()
-		bullets[i].play(function(){return}).drawSprite()
+		bullets[i].move();
+		bullets[i].play(function(){return}).drawSprite();
 		//bullets[i].angle+=10
 		while(g1.collide(bullets[i].bottom) && bullets[i].vy>=0)
 		{
