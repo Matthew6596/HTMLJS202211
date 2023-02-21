56ee1592f5dfd30f3fce710f4b17f31162e5eb39
Matthew6596 <masatterfield@email.neit.edu>
2023-02-20 21:30:38 -0500

    Added a normal webpage

---
+++ normal_webpage/script1.js
@@ -0,0 +1,6 @@
+document.addEventListener("click",onClick);
+
+function onClick(){
+    alert("You have broken the rules. You did the one thing I asked of you not to do. I am disappointed. Why would you do such a thing? I clearly told you not to. Was it because I told you not to? I told you not to for good reason, but now you have to suffer the consequences. I am forcing you to leave this webpage. If you choose to comeback, you can either be respectful of me and follow the rules, or choose to once again break the rules and be prompted with this same discouraging message, and the same shameful consequences. I hope you have learned something valuable from this experience. Sayonara.");
+    window.close();
+}
---
+++ normal_webpage/script2.js
@@ -0,0 +1,6 @@
+document.addEventListener("click",onClick);
+
+function onClick(){
+    alert("Wow. Okay. WOW. You broke the rules. I know the rule isn't stated explicitly on this page, but still, you should have known better. Come on, you're better than this. Rules are rules, and you are going to have to suffer the consequences. I'm sorry, but I really feel disrespected by this. It's ONE rule, just one. How hard can it really be?! Please have some decency, and don't come back. Sayonara.");
+    window.close();
+}
