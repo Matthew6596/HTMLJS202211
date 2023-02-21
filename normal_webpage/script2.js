3c8c5d7defaa6cbc34ffeb5b12d09eb80aa91fdd
Matthew6596 <masatterfield@email.neit.edu>
2023-02-20 21:43:47 -0500

    Updated the normal webpage

--- normal_webpage/script2.js
+++ normal_webpage/script2.js
@@ -1,6 +1,6 @@
-document.addEventListener("click",onClick);
+document.addEventListener("mousedown",onClick);
 
 function onClick(){
-    alert("Wow. Okay. WOW. You broke the rules. I know the rule isn't stated explicitly on this page, but still, you should have known better. Come on, you're better than this. Rules are rules, and you are going to have to suffer the consequences. I'm sorry, but I really feel disrespected by this. It's ONE rule, just one. How hard can it really be?! Please have some decency, and don't come back. Sayonara.");
+    alert("You broke the rules! I said NO CLICKING!!!!!! You shouldn't even be at this webpage at all! Seriously, how did you find this? Whatever, just leave (please)!");
     window.close();
 }
