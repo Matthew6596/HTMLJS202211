9cd07c1f1d8d18678aacdd898ce9444061a1e36d
Matthew6596 <masatterfield@email.neit.edu>
2023-03-31 15:04:51 -0400

    mostly level2 done

--- gds137/level2/script.js
+++ gds137/level2/script.js
@@ -8,7 +8,6 @@ document.addEventListener("keydown",keyDown);
 document.addEventListener("keyup",keyUp);
 
 function keyDown(e){
-    console.log(e.key);
     if(e.key=="s"){p1.vy=8;}
     if(e.key=="w"){p1.vy=-8;}
 }
@@ -19,12 +18,42 @@ function keyUp(e){
 var p1 = new Obj("joe",0,canvas.height/2,20,100,"rect",0,0);
 p1.bouncy = 0;
 
+var ball = new Obj("Prof. Ball",canvas.width/2,canvas.height/2,20,20,"circle",5,5);
+ball.bounded = false;
+ball.color = "blue";
+ball.vx*=-1;
+ball.vy = 0;
+
 /*-------------Main------------*/
 function main(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
     //
 
     p1.move();
-    p1.draw();
+    ball.move();
+    ball.bounceIn(-100,0,canvas.width,canvas.height);
+    if(ball.x<-ball.radius){ball.x=canvas.width/2;}
+    paddleCollision();
 
+    p1.draw();
+    ball.draw();
+
+}
+
+function paddleCollision(){
+    if(ball.x<p1.x+p1.radius+ball.radius){
+        if(ball.y>p1.y-p1.height/2-ball.radius&&ball.y<p1.y+p1.height/2+ball.radius){
+
+            ball.x = p1.x+p1.radius+ball.radius;
+            ball.vx *= -1;
+
+            if(ball.y>p1.y+p1.height/6){
+                ball.vy = 5;
+            }
+            else if(ball.y<p1.y-p1.height/6){
+                ball.vy = -5;
+            }
+            
+        }
+    }
 }
