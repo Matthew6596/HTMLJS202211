72dcd62fb3934487dc44eac38b58ca5b7dd419ef
Matthew6596 <masatterfield@email.neit.edu>
2023-04-19 12:38:29 -0400

    added collision and stuff to game jam game

--- gameDevJS_2023Jam/game.js
+++ gameDevJS_2023Jam/game.js
@@ -74,9 +74,13 @@ function onUserClick(){
 /*-----------------------------------UTILITY-----------------------------------*/
 
 function randNum(low, high){return Math.random()*(high-low)+low;}
-function randInt(lo, hi){return Math.round(randNum(lo, hi))}
+function randInt(lo, hi){return Math.round(randNum(lo, hi));}
 function getDist(x1,y1,x2,y2){return Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));}
-function percent(chance){ return (randNum(0,100)<=chance)}
+function percent(chance){ return (randNum(0,100)<=chance);}
+
+function getCollisionBorder(other){
+    return {left:other.left-1,right:other.right+1,top:other.top-1,bottom:other.bottom+1};
+}
 
 /*-----------------------------------OBJECTS-----------------------------------*/
 function Obj(x=0,y=0,w=100,h=100){
@@ -98,8 +102,8 @@ function Obj(x=0,y=0,w=100,h=100){
     this.bottom = this.y+this.height/2;
 
     this.draw = function(shape="rect"){
-        var halfW = this.width/2
-        var halfH = this.height/2
+        var halfW = this.width/2;
+        var halfH = this.height/2;
         if(shape=="rect"){
             ctx.save();
             ctx.translate(this.x,this.y);
@@ -111,7 +115,7 @@ function Obj(x=0,y=0,w=100,h=100){
             ctx.fillStyle = this.color;
             ctx.beginPath();
             ctx.translate(this.x,this.y);
-            ctx.arc(0,0,halfW,0,Math.PI/180,true)
+            ctx.arc(0,0,halfW,0,Math.PI/180,true);
             ctx.closePath();
             ctx.fill();
             ctx.restore();
@@ -157,17 +161,95 @@ function Obj(x=0,y=0,w=100,h=100){
         this.top = this.y-this.height/2;
         this.bottom = this.y+this.height/2;
     }
+
+    this.collides = function(other){
+        return ((this.right>=other.left)
+        &&(this.left<=other.right)
+        &&(this.bottom>=other.top)
+        &&(this.top<=other.bottom));
+    }
+
+    this.collision = function(other){
+        if(this.collides(other)){
+            var a_temp = [
+                Math.abs(this.right-other.left),
+                Math.abs(this.left-other.right),
+                Math.abs(this.bottom-other.top),
+                Math.abs(this.top-other.bottom),
+            ];
+            a_temp.sort(function(a, b){return b-a});
+
+            switch(a_temp[3]){
+                case(Math.abs(this.right-other.left)):
+                while(this.right>other.left){
+                    this.x--;
+                    this.right = this.x+this.width/2;
+                }
+                this.vx=0;
+                break;
+                case(Math.abs(this.left-other.right)):
+                while(this.left<other.right){
+                    this.x++;
+                    this.left = this.x-this.width/2;
+                }
+                this.vx=0;
+                break;
+                case(Math.abs(this.bottom-other.top)):
+                while(this.bottom>other.top){
+                    this.y--;
+                    this.bottom = this.y+this.height/2;
+                }
+                this.vy=0;
+                break;
+                case(Math.abs(this.top-other.bottom)):
+                while(this.top<other.bottom){
+                    this.y++;
+                    this.top = this.y-this.height/2;
+                }
+                this.vy=0;
+                break;
+            }
+
+            this.refreshColPoints();
+                
+        }
+    }
 }
 /*-----------------------------------VARIABLES-----------------------------------*/
 var player = new Obj(100,100,20,20);
 player.force = 0.6;
 player.friction = 0.85;
 player.controls = true;
+player.color = "rgb(50,210,50)";
+
+var wall1 = new Obj(400,400,40,200);
+wall1.color = "dimgrey";
+
+var a_objects = [player,wall1];
+var a_collides = [wall1];
 
 /*-----------------------------------MAIN-----------------------------------*/
 function main(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
     //
+
+    //Moving
     player.move();
-    player.draw();
+
+    //Doing Stuff
+    if(player.collides(getCollisionBorder(wall1))){
+        player.color = "red";
+    }else{
+        player.color = "rgb(50,210,50)";
+    }
+
+    //Player Collision
+    for(var i=0; i<a_collides.length; i++){
+        player.collision(a_collides[i]);
+    }
+
+    //Drawing
+    for(var i=0; i<a_objects.length; i++){
+        a_objects[i].draw();
+    }
 }
